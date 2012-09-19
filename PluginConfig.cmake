a28d610efb1af8669feb8cde9738aea8626b7a17
Sergey Radionov <RSATom@gmail.com>
2012-09-19 22:08:21 +0700

    it's time for 0.0.1.0

--- PluginConfig.cmake
+++ PluginConfig.cmake
@@ -8,7 +8,7 @@
 set(PLUGIN_NAME "FBVLC")
 set(PLUGIN_PREFIX "FBVLC")
 set(COMPANY_NAME "RSATom")
-set(PLUGIN_VERSION "0.0.0.8")
+set(PLUGIN_VERSION "0.0.1.0")
 
 #if you want embed vlc into FBVLC distribution:
 # 1) place vlc into FBVLC\Win\WiX\vlc-${VLC_VERSION}
