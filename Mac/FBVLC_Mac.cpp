968b38a367666ce60d6e6216073f9e3764fc91ca
Sergey Radionov <RSATom@gmail.com>
2012-11-19 04:49:10 +0700

    mac core graphics draw

--- Mac/FBVLC_Mac.cpp
+++ Mac/FBVLC_Mac.cpp
@@ -13,5 +13,44 @@ FBVLC_Mac::~FBVLC_Mac()
 
 bool FBVLC_Mac::onCoreGraphicsDraw(FB::CoreGraphicsDraw *evt, FB::PluginWindowMacCG*)
 {
-    return false;
+    FB::Rect bounds(evt->bounds);
+    //FB::Rect clip(evt->clip);
+    short width = bounds.right - bounds.left, height = bounds.bottom - bounds.top;
+
+    CGContextRef cgContext(evt->context);
+
+    CGContextSaveGState(cgContext);
+
+    CGContextTranslateCTM(cgContext, 0.0, height);
+    CGContextScaleCTM(cgContext, 1.0, -1.0);
+
+    CGColorSpaceRef cSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextSetFillColorSpace(cgContext, cSpace);
+
+    // Clear the stage.
+    //CGRect rect0 = { {0,0}, {width, height} };
+    //CGContextClearRect(cgContext, rect0);
+
+    if ( m_frame_buf.size() &&
+         m_frame_buf.size() >= m_media_width * m_media_height * DEF_PIXEL_BYTES )
+    {
+        CGContextRef bmpCtx =
+            CGBitmapContextCreate(&m_frame_buf[0], m_media_width, m_media_height, 8,
+                                  m_media_width * DEF_PIXEL_BYTES, cSpace, kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Little);
+        CGImageRef img = CGBitmapContextCreateImage(bmpCtx);
+
+        CGRect imgRect = {
+            { (width - m_media_width) / 2, (height - m_media_height) / 2 },
+            { m_media_width, m_media_height }
+        };
+        CGContextDrawImage(cgContext, imgRect, img);
+
+        CGImageRelease(img);
+        CGContextRelease(bmpCtx);
+    }
+
+    CGColorSpaceRelease(cSpace);
+    CGContextRestoreGState(cgContext);
+
+    return true; // This is handled
 }
