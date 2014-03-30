607f44cbbc8203dc8129ab24d0b8e563d075c777
Sergey Radionov <RSATom@gmail.com>
2014-03-30 08:45:09 +0700

    Mac: lua scripts added to bundle

--- Mac/projectDef.cmake
+++ Mac/projectDef.cmake
@@ -17,8 +17,14 @@ set( VLC_PATH "${CMAKE_CURRENT_SOURCE_DIR}/Mac/VLC-${VLC_VERSION}.app/Contents/M
 
 file( GLOB LIBVLC_LIB ${VLC_PATH}/lib/[^.]*.dylib )
 file( GLOB LIBVLC_PLUGINS ${VLC_PATH}/plugins/[^.]*.dylib )
+file( GLOB LIBVLC_LUA_EXTENSIONS ${VLC_PATH}/share/lua/extensions/[^.]*.luac )
+file( GLOB LIBVLC_LUA_MODULES ${VLC_PATH}/share/lua/modules/[^.]*.luac )
+file( GLOB LIBVLC_LUA_PLAYLIST ${VLC_PATH}/share/lua/playlist/[^.]*.luac )
 
-set( LIBVLC ${LIBVLC_LIB} ${LIBVLC_PLUGINS} )
+set( LIBVLC
+     ${LIBVLC_LIB} ${LIBVLC_PLUGINS}
+     ${LIBVLC_LUA_EXTENSIONS} ${LIBVLC_LUA_MODULES} ${LIBVLC_LUA_PLAYLIST}
+)
 
 set_source_files_properties(
     ${LIBVLC_LIB}
@@ -30,6 +36,24 @@ set_source_files_properties(
     ${LIBVLC_PLUGINS}
     PROPERTIES
     MACOSX_PACKAGE_LOCATION MacOS/plugins
+)
+
+set_source_files_properties(
+    ${LIBVLC_LUA_EXTENSIONS}
+    PROPERTIES
+    MACOSX_PACKAGE_LOCATION MacOS/share/lua/extensions/
+)
+
+set_source_files_properties(
+    ${LIBVLC_LUA_MODULES}
+    PROPERTIES
+    MACOSX_PACKAGE_LOCATION MacOS/share/lua/modules/
+)
+
+set_source_files_properties(
+    ${LIBVLC_LUA_PLAYLIST}
+    PROPERTIES
+    MACOSX_PACKAGE_LOCATION MacOS/share/lua/playlist/
 )
 
 include_directories( ${VLC_PATH}/include )
