325fd52487464b4db3dd780d36f50709cd04a5a2
Sergey Radionov <RSATom@gmail.com>
2014-06-29 18:44:34 +0700

    prep2008.bat removed

--- prep2008.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for %%i in ("%~0") do set THIS_BAT_PATH=%%~dpi
-set SAVE_CD=%CD%
-cd %THIS_BAT_PATH%\deps\firebreath\
-call prep2008.cmd "..\..\src" "..\..\build_win"
-cd %SAVE_CD%
-pause
